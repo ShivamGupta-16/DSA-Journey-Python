65d5809276cc6225e1ce0421b26091503cf1ec97
Shivam Gupta <145375475+ShivamGupta-16@users.noreply.github.com>
2025-02-27 01:45:11 +0530

    Delete bubbleSort.py

--- bubbleSort.py
+++
@@ -1,9 +0,0 @@
-def bubble(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(n-i-1):
-            if arr[j]> arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    print(arr)
-arr= [4223,4324,23,42,34,23,4,2,34,32,4,3,4,4,23,23]
-bubble(arr)
